9062e1611464c4b4d25f0a2b1e2694659e395133
Chris Mackey <chris@ladybug.tools>
2023-01-13 13:50:18 -0500

    fix(version): Bump the compatible version of EnergyPlus and OpenStudio

--- lbt_recipes/version.py
+++ lbt_recipes/version.py
@@ -8,8 +8,8 @@ from honeybee_energy.config import folders as energy_folders
 # These constants set the engine version compatibility across the entire
 # LBT Grasshopper plugin
 RADIANCE_DATE = (2021, 3, 28)
-EP_VERSION = (22, 1, 0)
-OS_VERSION = (3, 4, 0)
+EP_VERSION = (22, 2, 0)
+OS_VERSION = (3, 5, 0)
 COMPATIBILITY_URL = 'https://github.com/ladybug-tools/lbt-grasshopper/wiki/' \
     '1.4-Compatibility-Matrix'
 
