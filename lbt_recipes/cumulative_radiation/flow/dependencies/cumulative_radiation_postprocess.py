ffb6e37f277f56cd89e100e9fe8095b22ab552df
Mikkel Pedersen <mkklpdrsn@gmail.com>
2023-02-17 22:18:25 +0100

    fix(recipe): Re-export cumulative-radiation

---
+++ lbt_recipes/cumulative_radiation/flow/dependencies/cumulative_radiation_postprocess.py
@@ -0,0 +1,117 @@
+"""
+This file is auto-generated from cumulative-radiation:0.3.8.
+It is unlikely that you should be editing this file directly.
+Try to edit the original recipe itself and regenerate the code.
+
+Contact the recipe maintainers with additional questions.
+    mostapha: mostapha@ladybug.tools
+    ladybug-tools: info@ladybug.tools
+
+This file is licensed under "PolyForm Shield License 1.0.0".
+See https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt for more information.
+"""
+
+
+import luigi
+import pathlib
+from queenbee_local import QueenbeeTask
+from queenbee_local import load_input_param as qb_load_input_param
+from . import _queenbee_status_lock_
+
+
+_default_inputs = {   'average_irradiance': None,
+    'grid_name': None,
+    'params_folder': '__params',
+    'simulation_folder': '.',
+    'timestep': 1,
+    'wea': None}
+
+
+class AccumulateResults(QueenbeeTask):
+    """Postprocess average irradiance (W/m2) into cumulative radiation (kWh/m2)."""
+
+    # DAG Input parameters
+    _input_params = luigi.DictParameter()
+    _status_lock = _queenbee_status_lock_
+
+    # Task inputs
+    @property
+    def name(self):
+        return self._input_params['grid_name']
+
+    @property
+    def timestep(self):
+        return self._input_params['timestep']
+
+    @property
+    def average_irradiance(self):
+        value = pathlib.Path(self._input_params['average_irradiance'])
+        return value.as_posix() if value.is_absolute() \
+            else pathlib.Path(self.initiation_folder, value).resolve().as_posix()
+
+    @property
+    def wea(self):
+        value = pathlib.Path(self._input_params['wea'])
+        return value.as_posix() if value.is_absolute() \
+            else pathlib.Path(self.initiation_folder, value).resolve().as_posix()
+
+    @property
+    def execution_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def initiation_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def params_folder(self):
+        return pathlib.Path(self.execution_folder, self._input_params['params_folder']).resolve().as_posix()
+
+    def command(self):
+        return 'honeybee-radiance post-process cumulative-radiation avg_irr.mtx weather.wea --timestep {timestep} --output radiation.mtx'.format(timestep=self.timestep)
+
+    def output(self):
+        return {
+            'radiation': luigi.LocalTarget(
+                pathlib.Path(self.execution_folder, 'results/cumulative_radiation/{name}.res'.format(name=self.name)).resolve().as_posix()
+            )
+        }
+
+    @property
+    def input_artifacts(self):
+        return [
+            {'name': 'average_irradiance', 'to': 'avg_irr.mtx', 'from': self.average_irradiance, 'optional': False},
+            {'name': 'wea', 'to': 'weather.wea', 'from': self.wea, 'optional': False}]
+
+    @property
+    def output_artifacts(self):
+        return [
+            {
+                'name': 'radiation', 'from': 'radiation.mtx',
+                'to': pathlib.Path(self.execution_folder, 'results/cumulative_radiation/{name}.res'.format(name=self.name)).resolve().as_posix(),
+                'optional': False,
+                'type': 'file'
+            }]
+
+    @property
+    def task_image(self):
+        return 'docker.io/ladybugtools/honeybee-radiance:1.64.140'
+
+    @property
+    def image_workdir(self):
+        return '/home/ladybugbot/run'
+
+
+class _CumulativeRadiationPostprocess_f645822fOrchestrator(luigi.WrapperTask):
+    """Runs all the tasks in this module."""
+    # user input for this module
+    _input_params = luigi.DictParameter()
+
+    @property
+    def input_values(self):
+        params = dict(_default_inputs)
+        params.update(dict(self._input_params))
+        return params
+
+    def requires(self):
+        yield [AccumulateResults(_input_params=self.input_values)]
---
+++ lbt_recipes/cumulative_radiation/flow/dependencies/cumulative_radiation_prepare_folder.py
@@ -0,0 +1,493 @@
+"""
+This file is auto-generated from cumulative-radiation:0.3.8.
+It is unlikely that you should be editing this file directly.
+Try to edit the original recipe itself and regenerate the code.
+
+Contact the recipe maintainers with additional questions.
+    mostapha: mostapha@ladybug.tools
+    ladybug-tools: info@ladybug.tools
+
+This file is licensed under "PolyForm Shield License 1.0.0".
+See https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt for more information.
+"""
+
+
+import luigi
+import pathlib
+from queenbee_local import QueenbeeTask
+from queenbee_local import load_input_param as qb_load_input_param
+from . import _queenbee_status_lock_
+
+
+_default_inputs = {   'cpu_count': 50,
+    'grid_filter': '*',
+    'min_sensor_count': 500,
+    'model': None,
+    'north': 0.0,
+    'params_folder': '__params',
+    'radiance_parameters': '-ab 2 -ad 5000 -lw 2e-05',
+    'simulation_folder': '.',
+    'sky_density': 1,
+    'timestep': 1,
+    'wea': None}
+
+
+class CreateRadFolder(QueenbeeTask):
+    """Create a Radiance folder from a HBJSON input file."""
+
+    # DAG Input parameters
+    _input_params = luigi.DictParameter()
+    _status_lock = _queenbee_status_lock_
+
+    # Task inputs
+    @property
+    def grid_filter(self):
+        return self._input_params['grid_filter']
+
+    @property
+    def input_model(self):
+        value = pathlib.Path(self._input_params['model'])
+        return value.as_posix() if value.is_absolute() \
+            else pathlib.Path(self.initiation_folder, value).resolve().as_posix()
+
+    @property
+    def execution_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def initiation_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def params_folder(self):
+        return pathlib.Path(self.execution_folder, self._input_params['params_folder']).resolve().as_posix()
+
+    def command(self):
+        return 'honeybee-radiance translate model-to-rad-folder model.hbjson --grid "{grid_filter}" --grid-check'.format(grid_filter=self.grid_filter)
+
+    def output(self):
+        return {
+            'model_folder': luigi.LocalTarget(
+                pathlib.Path(self.execution_folder, 'model').resolve().as_posix()
+            ),
+            
+            'bsdf_folder': luigi.LocalTarget(
+                pathlib.Path(self.execution_folder, 'model/bsdf').resolve().as_posix()
+            ),
+            
+            'sensor_grids_file': luigi.LocalTarget(
+                pathlib.Path(self.execution_folder, 'results/average_irradiance/grids_info.json').resolve().as_posix()
+            )
+        }
+
+    @property
+    def input_artifacts(self):
+        return [
+            {'name': 'input_model', 'to': 'model.hbjson', 'from': self.input_model, 'optional': False}]
+
+    @property
+    def output_artifacts(self):
+        return [
+            {
+                'name': 'model-folder', 'from': 'model',
+                'to': pathlib.Path(self.execution_folder, 'model').resolve().as_posix(),
+                'optional': False,
+                'type': 'folder'
+            },
+                
+            {
+                'name': 'bsdf-folder', 'from': 'model/bsdf',
+                'to': pathlib.Path(self.execution_folder, 'model/bsdf').resolve().as_posix(),
+                'optional': True,
+                'type': 'folder'
+            },
+                
+            {
+                'name': 'sensor-grids-file', 'from': 'model/grid/_info.json',
+                'to': pathlib.Path(self.execution_folder, 'results/average_irradiance/grids_info.json').resolve().as_posix(),
+                'optional': False,
+                'type': 'file'
+            }]
+
+    @property
+    def task_image(self):
+        return 'docker.io/ladybugtools/honeybee-radiance:1.64.140'
+
+    @property
+    def image_workdir(self):
+        return '/home/ladybugbot/run'
+
+
+class CreateSky(QueenbeeTask):
+    """Generate a sun-up sky matrix."""
+
+    # DAG Input parameters
+    _input_params = luigi.DictParameter()
+    _status_lock = _queenbee_status_lock_
+
+    # Task inputs
+    @property
+    def north(self):
+        return self._input_params['north']
+
+    @property
+    def sky_type(self):
+        return 'total'
+
+    @property
+    def output_type(self):
+        return 'solar'
+
+    @property
+    def output_format(self):
+        return 'ASCII'
+
+    @property
+    def sky_density(self):
+        return self._input_params['sky_density']
+
+    @property
+    def cumulative(self):
+        return 'cumulative'
+
+    sun_up_hours = luigi.Parameter(default='all-hours')
+
+    @property
+    def wea(self):
+        value = pathlib.Path(self._input_params['wea'])
+        return value.as_posix() if value.is_absolute() \
+            else pathlib.Path(self.initiation_folder, value).resolve().as_posix()
+
+    @property
+    def execution_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def initiation_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def params_folder(self):
+        return pathlib.Path(self.execution_folder, self._input_params['params_folder']).resolve().as_posix()
+
+    def command(self):
+        return 'honeybee-radiance sky mtx sky.wea --name sky --north {north} --sky-type {sky_type} --{cumulative} --{sun_up_hours} --{output_type} --output-format {output_format} --sky-density {sky_density}'.format(sun_up_hours=self.sun_up_hours, north=self.north, output_format=self.output_format, sky_density=self.sky_density, sky_type=self.sky_type, output_type=self.output_type, cumulative=self.cumulative)
+
+    def output(self):
+        return {
+            'sky_matrix': luigi.LocalTarget(
+                pathlib.Path(self.execution_folder, 'resources/sky.mtx').resolve().as_posix()
+            )
+        }
+
+    @property
+    def input_artifacts(self):
+        return [
+            {'name': 'wea', 'to': 'sky.wea', 'from': self.wea, 'optional': False}]
+
+    @property
+    def output_artifacts(self):
+        return [
+            {
+                'name': 'sky-matrix', 'from': 'sky.mtx',
+                'to': pathlib.Path(self.execution_folder, 'resources/sky.mtx').resolve().as_posix(),
+                'optional': False,
+                'type': 'file'
+            }]
+
+    @property
+    def task_image(self):
+        return 'docker.io/ladybugtools/honeybee-radiance:1.64.140'
+
+    @property
+    def image_workdir(self):
+        return '/home/ladybugbot/run'
+
+
+class CreateSkyDome(QueenbeeTask):
+    """Create a skydome for daylight coefficient studies."""
+
+    # DAG Input parameters
+    _input_params = luigi.DictParameter()
+    _status_lock = _queenbee_status_lock_
+
+    # Task inputs
+    @property
+    def sky_density(self):
+        return self._input_params['sky_density']
+
+    @property
+    def execution_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def initiation_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def params_folder(self):
+        return pathlib.Path(self.execution_folder, self._input_params['params_folder']).resolve().as_posix()
+
+    def command(self):
+        return 'honeybee-radiance sky skydome --name rflux_sky.sky --sky-density {sky_density}'.format(sky_density=self.sky_density)
+
+    def output(self):
+        return {
+            'sky_dome': luigi.LocalTarget(
+                pathlib.Path(self.execution_folder, 'resources/sky.dome').resolve().as_posix()
+            )
+        }
+
+    @property
+    def output_artifacts(self):
+        return [
+            {
+                'name': 'sky-dome', 'from': 'rflux_sky.sky',
+                'to': pathlib.Path(self.execution_folder, 'resources/sky.dome').resolve().as_posix(),
+                'optional': False,
+                'type': 'file'
+            }]
+
+    @property
+    def task_image(self):
+        return 'docker.io/ladybugtools/honeybee-radiance:1.64.140'
+
+    @property
+    def image_workdir(self):
+        return '/home/ladybugbot/run'
+
+
+class CopyGridInfo(QueenbeeTask):
+    """Copy a file or folder to a destination."""
+
+    # DAG Input parameters
+    _input_params = luigi.DictParameter()
+    _status_lock = _queenbee_status_lock_
+
+    # Task inputs
+    @property
+    def src(self):
+        value = pathlib.Path(self.input()['CreateRadFolder']['sensor_grids_file'].path)
+        return value.as_posix() if value.is_absolute() \
+            else pathlib.Path(self.initiation_folder, value).resolve().as_posix()
+
+    @property
+    def execution_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def initiation_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def params_folder(self):
+        return pathlib.Path(self.execution_folder, self._input_params['params_folder']).resolve().as_posix()
+
+    def command(self):
+        return 'echo copying input path...'
+
+    def requires(self):
+        return {'CreateRadFolder': CreateRadFolder(_input_params=self._input_params)}
+
+    def output(self):
+        return {
+            'dst': luigi.LocalTarget(
+                pathlib.Path(self.execution_folder, 'results/cumulative_radiation/grids_info.json').resolve().as_posix()
+            )
+        }
+
+    @property
+    def input_artifacts(self):
+        return [
+            {'name': 'src', 'to': 'input_path', 'from': self.src, 'optional': False}]
+
+    @property
+    def output_artifacts(self):
+        return [
+            {
+                'name': 'dst', 'from': 'input_path',
+                'to': pathlib.Path(self.execution_folder, 'results/cumulative_radiation/grids_info.json').resolve().as_posix(),
+                'optional': False,
+                'type': 'folder'
+            }]
+
+    @property
+    def task_image(self):
+        return 'docker.io/python:3.7-slim'
+
+    @property
+    def image_workdir(self):
+        return '/home/ladybugbot/run'
+
+
+class CreateOctree(QueenbeeTask):
+    """Generate an octree from a Radiance folder."""
+
+    # DAG Input parameters
+    _input_params = luigi.DictParameter()
+    _status_lock = _queenbee_status_lock_
+
+    # Task inputs
+    black_out = luigi.Parameter(default='default')
+
+    include_aperture = luigi.Parameter(default='include')
+
+    @property
+    def model(self):
+        value = pathlib.Path(self.input()['CreateRadFolder']['model_folder'].path)
+        return value.as_posix() if value.is_absolute() \
+            else pathlib.Path(self.initiation_folder, value).resolve().as_posix()
+
+    @property
+    def execution_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def initiation_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def params_folder(self):
+        return pathlib.Path(self.execution_folder, self._input_params['params_folder']).resolve().as_posix()
+
+    def command(self):
+        return 'honeybee-radiance octree from-folder model --output scene.oct --{include_aperture}-aperture --{black_out}'.format(include_aperture=self.include_aperture, black_out=self.black_out)
+
+    def requires(self):
+        return {'CreateRadFolder': CreateRadFolder(_input_params=self._input_params)}
+
+    def output(self):
+        return {
+            'scene_file': luigi.LocalTarget(
+                pathlib.Path(self.execution_folder, 'resources/scene.oct').resolve().as_posix()
+            )
+        }
+
+    @property
+    def input_artifacts(self):
+        return [
+            {'name': 'model', 'to': 'model', 'from': self.model, 'optional': False}]
+
+    @property
+    def output_artifacts(self):
+        return [
+            {
+                'name': 'scene-file', 'from': 'scene.oct',
+                'to': pathlib.Path(self.execution_folder, 'resources/scene.oct').resolve().as_posix(),
+                'optional': False,
+                'type': 'file'
+            }]
+
+    @property
+    def task_image(self):
+        return 'docker.io/ladybugtools/honeybee-radiance:1.64.140'
+
+    @property
+    def image_workdir(self):
+        return '/home/ladybugbot/run'
+
+
+class SplitGridFolder(QueenbeeTask):
+    """Create new sensor grids folder with evenly distributed sensors.
+
+    This function creates a new folder with evenly distributed sensor grids. The folder
+    will include a ``_redist_info.json`` file which has the information to recreate the
+    original input files from this folder and the results generated based on the grids
+    in this folder."""
+
+    # DAG Input parameters
+    _input_params = luigi.DictParameter()
+    _status_lock = _queenbee_status_lock_
+
+    # Task inputs
+    @property
+    def cpu_count(self):
+        return self._input_params['cpu_count']
+
+    @property
+    def cpus_per_grid(self):
+        return '1'
+
+    @property
+    def min_sensor_count(self):
+        return self._input_params['min_sensor_count']
+
+    @property
+    def input_folder(self):
+        value = pathlib.Path(self.input()['CreateRadFolder']['model_folder'].path, 'grid')
+        return value.as_posix() if value.is_absolute() \
+            else pathlib.Path(self.initiation_folder, value).resolve().as_posix()
+
+    @property
+    def execution_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def initiation_folder(self):
+        return pathlib.Path(self._input_params['simulation_folder']).as_posix()
+
+    @property
+    def params_folder(self):
+        return pathlib.Path(self.execution_folder, self._input_params['params_folder']).resolve().as_posix()
+
+    def command(self):
+        return 'honeybee-radiance grid split-folder ./input_folder ./output_folder {cpu_count} --grid-divisor {cpus_per_grid} --min-sensor-count {min_sensor_count}'.format(min_sensor_count=self.min_sensor_count, cpu_count=self.cpu_count, cpus_per_grid=self.cpus_per_grid)
+
+    def requires(self):
+        return {'CreateRadFolder': CreateRadFolder(_input_params=self._input_params)}
+
+    def output(self):
+        return {
+            'output_folder': luigi.LocalTarget(
+                pathlib.Path(self.execution_folder, 'resources/grid').resolve().as_posix()
+            ),
+            
+            'dist_info': luigi.LocalTarget(
+                pathlib.Path(self.execution_folder, 'initial_results/_redist_info.json').resolve().as_posix()
+            )
+        }
+
+    @property
+    def input_artifacts(self):
+        return [
+            {'name': 'input_folder', 'to': 'input_folder', 'from': self.input_folder, 'optional': False}]
+
+    @property
+    def output_artifacts(self):
+        return [
+            {
+                'name': 'output-folder', 'from': 'output_folder',
+                'to': pathlib.Path(self.execution_folder, 'resources/grid').resolve().as_posix(),
+                'optional': False,
+                'type': 'folder'
+            },
+                
+            {
+                'name': 'dist-info', 'from': 'output_folder/_redist_info.json',
+                'to': pathlib.Path(self.execution_folder, 'initial_results/_redist_info.json').resolve().as_posix(),
+                'optional': False,
+                'type': 'file'
+            }]
+
+    @property
+    def task_image(self):
+        return 'docker.io/ladybugtools/honeybee-radiance:1.64.140'
+
+    @property
+    def image_workdir(self):
+        return '/home/ladybugbot/run'
+
+
+class _CumulativeRadiationPrepareFolder_f645822fOrchestrator(luigi.WrapperTask):
+    """Runs all the tasks in this module."""
+    # user input for this module
+    _input_params = luigi.DictParameter()
+
+    @property
+    def input_values(self):
+        params = dict(_default_inputs)
+        params.update(dict(self._input_params))
+        return params
+
+    def requires(self):
+        yield [CreateSky(_input_params=self.input_values), CreateSkyDome(_input_params=self.input_values), CopyGridInfo(_input_params=self.input_values), CreateOctree(_input_params=self.input_values), SplitGridFolder(_input_params=self.input_values)]
